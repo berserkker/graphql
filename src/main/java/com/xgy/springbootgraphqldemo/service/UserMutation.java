fc68ac08317d78a0debba6eb7190789ab00a853f
徐光耀 <xuguangyao@xuguangyaodeMacBook-Pro.local>
2019-08-26 20:29:38 +0800

    增加输出

--- src/main/java/com/xgy/springbootgraphqldemo/service/UserMutation.java
+++ src/main/java/com/xgy/springbootgraphqldemo/service/UserMutation.java
@@ -11,34 +11,43 @@ import java.util.HashMap;
 public class UserMutation implements GraphQLMutationResolver {
     public static HashMap<Integer, User> userHashMap = UserQuery.userHashMap;
 
-    public boolean saveUser(User user){
+    public boolean saveUser(User user) {
         System.out.println("save");
-        userHashMap.put(user.getId(),user);
+        userHashMap.put(user.getId(), user);
+        printMap();
         return true;
     }
 
-    public boolean deleteUser(Integer id){
+    public boolean deleteUser(Integer id) {
         System.out.println("delete");
         userHashMap.remove(id);
+        printMap();
         return true;
     }
 
-    public boolean updateUser(User user){
+    public boolean updateUser(User user) {
         System.out.println("update");
         User localUser = userHashMap.get(user.getId());
-        if(user.getAge()>0){
+        if (user.getAge() > 0) {
             localUser.setAge(user.getAge());
         }
 
-        if(StringUtils.isNotEmpty(user.getUsername())){
+        if (StringUtils.isNotEmpty(user.getUsername())) {
             localUser.setUsername(user.getUsername());
         }
 
-        if(StringUtils.isNotEmpty(user.getPassword())){
+        if (StringUtils.isNotEmpty(user.getPassword())) {
             localUser.setPassword(user.getPassword());
         }
 
-        userHashMap.put(user.getId(),localUser);
+        userHashMap.put(user.getId(), localUser);
+        printMap();
         return true;
+    }
+
+    private void printMap() {
+        userHashMap.forEach((id, user) -> {
+            System.out.println(id + "---" + user.getUsername() + "--" + user.getAge());
+        });
     }
 }
